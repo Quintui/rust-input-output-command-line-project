c875d255e37e60f6dcffee27f3680cb48aab586e
Kristian Veter <rscrafter1234@icloud.com>
2022-06-29 17:11:46 +0300

    improvement(src): implement iterators

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,23 @@ use minigrep::Config;
 
 
 fn main() {
-    let args:  Vec<String> = env::args().collect();
+    let args= env::args();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+
+
+    let config = Config::new(args).unwrap_or_else(|err| {
         println!("Something went wrong {}", err);
         process::exit(1)
     });
 
     println!("In file {}", config.filename);
     println!("File query, '{}'", config.query);
- 
 
     if let Err(e) = Config::run(config) {
             println!("Application error: {}", e);
             process::exit(1)
     }
 
-   
-        
     
 }
 
